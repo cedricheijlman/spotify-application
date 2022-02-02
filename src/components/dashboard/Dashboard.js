2270364dabe3254c4cf1d5fbf3d144cfb04b03ac
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-02 14:11:19 +0100

    music player under screen

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./dashboard.css";
 import SpotifyWebApi from "spotify-web-api-js";
+import SpotifyPlayer from "react-spotify-web-playback";
+
 let spotifyApi = new SpotifyWebApi();
 
 function Dashboard({ accessKeyApi }) {
@@ -10,7 +12,17 @@ function Dashboard({ accessKeyApi }) {
     spotifyApi.setAccessToken(accessKeyApi);
   }, []);
 
-  return <div>{}</div>;
+  return (
+    <>
+      <div id="dashboard">
+        <div className="dashboard__left"></div>
+        <div className="dashboard__right"></div>
+        <div className="player">
+          <SpotifyPlayer token={accessKeyApi} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
