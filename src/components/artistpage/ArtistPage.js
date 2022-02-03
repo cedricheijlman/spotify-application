adc5aa893becf8ca4d649f06bb6ff470e514364a
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-03 22:57:24 +0100

    artist page info fetch

--- src/components/artistpage/ArtistPage.js
+++ src/components/artistpage/ArtistPage.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import useSpotifyWrapper from "../../useSpotifyWrapper";
 import "./artistpage.css";
 
 function ArtistPage() {
   // get Artist ID from url
   const artistId = window.location.pathname.slice(8);
-  console.log(artistId);
-  return <div>Artist Page</div>;
+
+  // Initialise Wrapper Spotify API
+  const accessKeyApi = sessionStorage.getItem("accessToken");
+  let spotifyApi = new SpotifyWebApi();
+
+  // Set AccessCode Spotify API
+  const setAccessCode = useSpotifyWrapper(accessKeyApi, spotifyApi);
+
+  // setState Artist
+  const [artist, setArtist] = useState(null);
+  const [artistTopTracks, setArtistTopTracks] = useState(null);
+  const [artistAlbums, setArtistAlbums] = useState(null);
+
+  useEffect(() => {
+    if (!artist) {
+      spotifyApi.getArtist(artistId, {}, (err, result) => {
+        setArtist(result);
+      });
+    }
+
+    if (!artistTopTracks) {
+      spotifyApi.getArtistTopTracks(artistId, "US", {}, (err, result) => {
+        setArtistTopTracks(result);
+      });
+    }
+
+    if (!artistAlbums) {
+      spotifyApi.getArtistAlbums(artistId, {}, (err, result) => {
+        setArtistAlbums(result);
+      });
+    }
+  }, [artistId]);
+
+  return (
+    <div id="artistPage">
+      {artist !== null && (
+        <>
+          <div className="artist__info">
+            <div>
+              <img src={artist.images[0] && artist.images[0].url} />
+            </div>
+            <div className="artist__infoText">
+              <p>{artist.type && artist.type}</p>
+              <h2>{artist.name && artist.name}</h2>
+              <p>{artist.followers && artist.followers.total} Followers</p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ArtistPage;
