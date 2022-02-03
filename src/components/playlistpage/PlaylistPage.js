d489b5adaf9c3f5d671abe74fbc0b996c9d99936
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-03 13:55:04 +0100

    show playlist tracks on playlistPage

--- src/components/playlistpage/PlaylistPage.js
+++ src/components/playlistpage/PlaylistPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import useSpotifyWrapper from "../../useSpotifyWrapper";
+import "./playlistpage.css";
 
 function PlaylistPage() {
   // Initialise Wrapper Spotify API
@@ -12,7 +13,7 @@ function PlaylistPage() {
 
   // setState Playlist
   const [playlist, setPlaylist] = useState(null);
-
+  const [playlistTracks, setPlaylistTracks] = useState(null);
   // get id playlist from url
   const playlistId = window.location.pathname.slice(10);
 
@@ -26,9 +27,46 @@ function PlaylistPage() {
     }
   }, [playlistId]);
 
+  useEffect(() => {
+    if (playlist) {
+      spotifyApi.getPlaylistTracks(playlistId, {}, (err, result) => {
+        setPlaylistTracks(result);
+        console.log("Playlist tracks", result);
+      });
+    }
+  }, [playlist]);
+
   return (
-    <div>
-      <h1>Playlist</h1>
+    <div id="playlistPage">
+      {playlist !== null && (
+        <>
+          <div className="playlistPage__info">
+            <img width={250} src={playlist.images[0].url} />
+            <div className="playlistPage__infoText">
+              <p className="playlistType">{playlist.type}</p>
+              <h2>{playlist.name}</h2>
+              <p>{playlist.description}</p>
+              <div className="playlistOwnerAndTracks">
+                <p>
+                  {playlist.owner.display_name} • {playlist.tracks.total} Tracks
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            {playlistTracks &&
+              playlistTracks.items.map((item) => {
+                return (
+                  <div>
+                    <h5>{item.track.name}</h5>
+                    <p>{item.track.artists[0].name}</p>
+                    <hr />
+                  </div>
+                );
+              })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
