e07436af23e96a888033b3a610eede6126f982ea
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-07 18:16:17 +0100

    discover page

--- src/components/discover/Discover.js
+++ src/components/discover/Discover.js
@@ -1,32 +1,41 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import SpotifyWebApi from "spotify-web-api-js";
 import { CurrentTrackContext } from "../../CurrentTrackContext";
 import useSpotifyWrapper from "../../useSpotifyWrapper";
+import AlbumCard from "../main/AlbumCard";
+import PlaylistCard from "../main/AlbumCard";
 import "./discover.css";
 function Discover() {
   const accessKeyApi = sessionStorage.getItem("accessToken");
   const { currentTrack, setCurrentTrack } = useContext(CurrentTrackContext);
+  const [currentOption, setCurrentOption] = useState("pop");
   const [limit, setLimit] = useState(10);
-  const [discover, setDiscover] = useState({
-    pop: null,
-    hiphop: null,
-    rock: null,
-    chill: null,
-    indie_alt: null,
-  });
+  const [popCategory, setPopCategory] = useState(null);
+  const [rockCategory, setRockCategory] = useState(null);
+
   // Initialize Wrapper and set AccessCode
   let spotifyApi = new SpotifyWebApi();
   const setAccessCode = useSpotifyWrapper(accessKeyApi, spotifyApi);
 
   // Get request Featured Playlists
   useEffect(() => {
-    if (!discover.pop) {
+    if (!popCategory) {
       spotifyApi.getCategoryPlaylists(
         "pop",
         { limit: 10, country: "us" },
         (err, result) => {
-          console.log(result);
-          setDiscover({ ...discover, pop: result.playlists.items });
+          setPopCategory(result.playlists.items);
+        }
+      );
+    }
+
+    if (!rockCategory) {
+      spotifyApi.getCategoryPlaylists(
+        "rock",
+        { limit: 10, country: "us" },
+        (err, result) => {
+          setRockCategory(result.playlists.items);
         }
       );
     }
@@ -35,14 +44,65 @@ function Discover() {
   return (
     <div id="discover">
       <h1>Discover</h1>
+      <section>
+        <a
+          onClick={() => {
+            setCurrentOption("pop");
+          }}
+        >
+          Pop
+        </a>
+        <a
+          onClick={() => {
+            setCurrentOption("rock");
+          }}
+        >
+          Rock
+        </a>
+        <a
+          onClick={() => {
+            setCurrentOption("indieRock");
+          }}
+        >
+          Indie Rock
+        </a>
+      </section>
       <section className="discoverRow">
-        <h2>Pop</h2>
-        <div className="discoverPlaylistsRow">
-          {discover.pop &&
-            discover.pop.map((playlist) => {
-              return <h1>{playlist.name}</h1>;
-            })}
-        </div>
+        {currentOption == "pop" && popCategory && (
+          <>
+            <h2>Pop</h2>
+            <div className="discoverPlaylistsRow">
+              {popCategory.map((playlist, index) => {
+                return (
+                  <Link key={index} to={`/playlist/${playlist.id}`}>
+                    <AlbumCard
+                      img={playlist.images[0].url}
+                      name={playlist.name}
+                    />
+                  </Link>
+                );
+              })}
+            </div>
+          </>
+        )}
+
+        {currentOption == "rock" && rockCategory && (
+          <>
+            <h2>rock</h2>
+            <div className="discoverPlaylistsRow">
+              {rockCategory.map((playlist, index) => {
+                return (
+                  <Link key={index} to={`/playlist/${playlist.id}`}>
+                    <AlbumCard
+                      img={playlist.images[0].url}
+                      name={playlist.name}
+                    />
+                  </Link>
+                );
+              })}
+            </div>
+          </>
+        )}
       </section>
     </div>
   );
