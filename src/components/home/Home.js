b2e102815030627769f98cec00cb8abdd5715ec9
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-07 16:39:43 +0100

    New Releases

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,12 +4,13 @@ import SpotifyWebApi from "spotify-web-api-js";
 import useSpotifyWrapper from "../../useSpotifyWrapper";
 import NewReleases from "./NewReleases";
 import { CurrentTrackContext } from "../../CurrentTrackContext";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import AlbumCard from "../main/AlbumCard";
 function Home() {
   const accessKeyApi = sessionStorage.getItem("accessToken");
   const [featuredPlaylists, setFeaturedPlaylists] = useState(null);
-  const [currentOption, setCurrentOption] = useState("featuredPlaylists");
+  const [currentOption, setCurrentOption] = useState("newReleases");
+  const [newReleases, setNewReleases] = useState(null);
   const { currentTrack, setCurrentTrack } = useContext(CurrentTrackContext);
 
   // Initialize Wrapper and set AccessCode
@@ -19,20 +20,28 @@ function Home() {
   // Get request Featured Playlists
   useEffect(() => {
     if (!featuredPlaylists) {
-      spotifyApi.getFeaturedPlaylists({ limit: 50 }, (err, data) => {
+      spotifyApi.getFeaturedPlaylists({ limit: 20 }, (err, data) => {
         setFeaturedPlaylists(data);
-        console.log(data);
       });
     }
 
-    spotifyApi.getCategoryPlaylists("pop", { limit: 50 }, (err, result) => {
-      console.log(result);
-    });
+    if (!newReleases) {
+      spotifyApi.getNewReleases({}, (err, result) => {
+        setNewReleases(result.albums.items);
+      });
+    }
   }, [spotifyApi]);
-
+  console.log(newReleases);
   return (
     <div id="home">
       <div className="home__choose">
+        <p
+          onClick={() => {
+            setCurrentOption("newReleases");
+          }}
+        >
+          New Releases
+        </p>
         <p
           onClick={() => {
             setCurrentOption("featuredPlaylists");
@@ -54,19 +63,36 @@ function Home() {
         >
           Editor's Picks
         </p>
-        <p
-          onClick={() => {
-            setCurrentOption("featured");
-          }}
-        >
-          Editor's Picks
-        </p>
       </div>
-      <div className="featuredPlaylistsRow">
-        {featuredPlaylists !== null && currentOption == "featuredPlaylists" && (
-          <NewReleases featuredPlaylists={featuredPlaylists} />
-        )}
-      </div>
+
+      {newReleases !== null && currentOption == "newReleases" && (
+        <>
+          <h1>New Releases</h1>
+          <div className="homeRow">
+            {newReleases.map((album) => {
+              return (
+                <Link to={`/album/${album.id}`}>
+                  <AlbumCard
+                    name={album.name}
+                    artist={album.artists[0].name}
+                    artistId={album.artists[0].id}
+                    img={album.images[0].url}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+        </>
+      )}
+
+      {featuredPlaylists !== null && currentOption == "featuredPlaylists" && (
+        <>
+          <h1>Featured Playlists</h1>
+          <div className="homeRow">
+            <NewReleases featuredPlaylists={featuredPlaylists} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/main/AlbumCard.js
+++ src/components/main/AlbumCard.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./artistcard.css";
-function AlbumCard({ name, img, releaseDate }) {
+function AlbumCard({ name, img, releaseDate, artist, artistId }) {
   return (
     <div className="artistAlbum__item">
       <img src={img} />
       <h5>{name}</h5>
-      <p>{releaseDate}</p>
+      {releaseDate && <p>{releaseDate}</p>}
+      {artist && artistId && (
+        <Link to={`artist/${artistId}`}>
+          <p
+            style={{ textAlign: "center", fontSize: "13px", fontWeight: "500" }}
+          >
+            {artist}
+          </p>
+        </Link>
+      )}
     </div>
   );
 }
